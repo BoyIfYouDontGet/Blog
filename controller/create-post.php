cff0ed849004ec566c15a4aa1355edf5d585e8a6
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-11-12 08:46:33 -0800

    established a database connection

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,9 +1,13 @@
 <?php
+require_once(__DIR__ . "/../model/database.php");
+$connection = new mysqli($host, $username, $password, $database)
 //store information under a variable
 $title =filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING) ;
 // in parenthesis it tells us where to get our information
 $post = filter_input(INPUT_POST,"post",FILTER_SANITIZE_STRING);
 // the same thing but instead of title its posts
 echo "<p>Title:$title</p>";
+// echoes the title
 echo "<p>Post:$post</p>";
-?>
+// echoes thepost
+$connection->close();
