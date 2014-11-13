70eeeda3d5894c47320a03441f7b61765e0dd19b
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-11-13 09:09:34 -0800

    querying my database to submit a blog post

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,13 +1,20 @@
 <?php
 require_once(__DIR__ . "/../model/database.php");
-$connection = new mysqli($host, $username, $password, $database)
+$connection = new mysqli($host, $username, $password, $database);
 //store information under a variable
 $title =filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING) ;
 // in parenthesis it tells us where to get our information
 $post = filter_input(INPUT_POST,"post",FILTER_SANITIZE_STRING);
 // the same thing but instead of title its posts
-echo "<p>Title:$title</p>";
-// echoes the title
-echo "<p>Post:$post</p>";
+$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
 // echoes thepost
+if($query) {
+	echo "<p>Successfully inserted post : $title</p>";	
+}
+// if query wasnt successful the else statement would be done
+else {
+	echo "<p>$connection->error</p>";
+}
+
+// creates a query to connect you to the database
 $connection->close();
