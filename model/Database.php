cc37edcf71f381c99dab8dc4b1ec20430da27440
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-11-14 09:18:38 -0800

    created a new file

---
+++ model/Database.php
@@ -0,0 +1,14 @@
+<?php 
+// to create a class, you put classs then you put the name of the classs afterwards.
+// the class i am creating is going to take repeated code and make our code easier to read. A class is an object. A class can have variables inside. Classes store information so you can call it later. 
+class Database {
+private $connection;
+private $host;
+private $password;
+private $username;
+private $database;
+}
+
+
+
+?>
