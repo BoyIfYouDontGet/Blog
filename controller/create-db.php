272f90236b2a45c7a764e75f0f3f037ab670b753
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-03 08:56:28 -0800

    Created a table for users and changed some css. Also created a new file

--- controller/create-db.php
+++ controller/create-db.php
@@ -20,4 +20,20 @@ else {
 	echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
 // added paragraph tags
+//creating new query using a session. Creating a table for the users
+$query = $_SESSION["connection"]->query("CREATE TABLE users("
+	 . "id int(11) NOT NULL AUTO_INCREMENT," 
+	 . "username varchar(30) NOT NULL,"
+	 . "email varchar(50) NOT NULL,"
+	 ."password char(128) NOT NULL,"
+	 . "salt char(128)NOT NULL,"
+	 . "PRIMARY KEY (id) )");
+// im going to check if this table works by runnig an if else statement
+if ($query) {
+	echo "<p>Successfully created table:users</p>";
+}
+else {
+	echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
+
 ?>
---
+++ view/Register-form.php
@@ -0,0 +1,23 @@
+
+
+<form>
+<div>  <label for="email"> Email: </label>
+<input type="text" name="email" />
+    </div>
+
+<div> <label for "username"> Username: </label>
+<input type="text" name="username" />
+   </div>
+
+<div>
+<label for="password"> Password:</label>
+<input type"password" name="password" />  
+   </div>
+<div>
+<button type="submit">Submit</button>
+</div>
+
+</form>
+ <?php
+// created divs so that the user needs to log in to post on my website. The 3 things they need to input are their email username and password. Then the submit button submits all the information
+ ?>
