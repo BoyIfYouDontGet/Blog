dfb78a6da282fa5576a5b7dbd1db22c0ac40f2fd
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-15 23:27:19 -0800

    created a login mechanism

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,6 +1,23 @@
 <?php
 require_once(__DIR__ . "/../model/config.php");
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
-$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING)
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 // this query selects a username and password that match the users information
 $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username' ");
+//this function checks to see if the login information matches the login information from our database
+if($query->num_rows == 1) {
+	$row = $query->fetch_array();
+// this if statement runs the login info in the database and the 3 equals signs check to see if the login info matches
+	if ($row["password"] === crypt($password, $row["salt"])) {
+		$_SESSION["authenticated"] = true;
+		echo "<p> Login Successful </p>";
+	}
+	//this else statement says there was a login in error if the login info doesnt match
+	else {
+		echo "Invalid username and password</p>";
+	}
+}
+	else {
+		echo "<p>Invalid useranme and password</p>";
+	}
+
--- model/config.php
+++ model/config.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . "/database.php");
 session_start();
-
+session_regenerate_id(true);
 $path     = "/BLOG/";
 //refracted the variable from database.php to this config.php. I refractured this code because it make it easier for us to maintain our website
 // variables
