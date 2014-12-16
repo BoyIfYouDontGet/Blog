8a177a28599032722321d162dbe595dc733746fe
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-15 22:15:30 -0800

    deleted the background and inserted code that shows me all of my posts from the beginning of time!

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ require_once(__DIR__ . "/view/navigation.php");
 // looks for a file
 	require_once (__DIR__ . "/controller/create-db.php");
 	require_once(__DIR__ . "/view/footer.php");
+	require_once(__DIR__ . "/controller/read-post.php");
 ?>
