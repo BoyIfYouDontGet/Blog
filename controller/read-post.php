a5f47a88185ae8382a96fdbbde0d4cd10363c549
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-16 23:59:18 -0800

    indented code

--- controller/read-post.php
+++ controller/read-post.php
@@ -1,16 +1,17 @@
 <?php
 require_once(__DIR__ . "/../model/config.php");
 // this query will show all of our posts from the beginning of time!
-$query = "SELECT * FROM posts";
-$result = $_SESSION["connection"]->query($query);
+        $query = "SELECT * FROM posts";
+        $result = $_SESSION["connection"]->query($query);
 //this if statement echoes out all of our titles and posts 
-if($result) {
-	while($row = mysqli_fetch_array($result)) {
-		echo "<div class='post'>";
-		echo "<h2>" . $row['title'] . "</h2>";
-		echo "<br />";
-		echo "<p>" . $row['post'] . "</h1>";
-		echo "<br/>";
-		echo "</div";
-	}
-}
+        if ($result) {
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<div class='post'>";
+                echo "<h2>" . $row['title'] . "</h2>";
+                echo "<br />";
+                echo "<p>" . $row['post'] . "</h1>";
+                echo "<br/>";
+                echo "</div";
+            }
+        }
+        
