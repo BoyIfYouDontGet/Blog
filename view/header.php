1d531934ee1cb36d19022784a4d14138dab1096d
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-16 23:49:20 -0800

    added modal

--- view/header.php
+++ view/header.php
@@ -3,6 +3,7 @@
 	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 	<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="CSS/main.css">
+	
 		<meta charset="UTF-8">
 		<title>Blog project</title>
 
@@ -11,3 +12,18 @@
 		<article>
 	<img width="500" height="auto" align="right" src="http://fc03.deviantart.net/fs70/f/2012/081/8/0/futuristic_city_4_by_rich35211-d4ssxt3.jpg">
 </article>
+
+
+
+
+
+<!-- Large modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Futuristic Modal!</button>
+
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      THE FUTURE IS HERE
+    </div>
+  </div>
+</div>
