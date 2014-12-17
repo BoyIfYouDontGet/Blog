dbbd9eec62ccb1b6c05a7734118b2513d01a8888
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-17 08:35:44 -0800

    jquery

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,6 @@
 
  
-
+         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script type="text/javascript" src"JS/bootstrap.min.css"></script>
 		<script type="text/javascript" src="JS/readmore.js"></script>
 		<script type="text/javascript" src="JS/main.js"></script>
