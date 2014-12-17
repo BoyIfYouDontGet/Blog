28c525f1361d7d60bb85a6364f2e07dba17c5c6c
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-16 23:58:03 -0800

    indented code

--- controller/logout-user.php
+++ controller/logout-user.php
@@ -1,8 +1,9 @@
 <?php
-// this file will log out oa user
-require_once(__DIR__ . "/../model/config.php");
+  // this file will log out oa user
+        require_once(__DIR__ . "/../model/config.php");
 
-unset($_SESSION["authenticated"]);
+        unset($_SESSION["authenticated"]);
 //destroys a session if the user is logging out
-session_destroy();
-header("Location: " . $path . "index.php");
+        session_destroy();
+        header("Location: " . $path . "index.php");
+        
