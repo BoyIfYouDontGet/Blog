2c3acf181871641160b9213772b970169ac3227f
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-17 00:16:36 -0800

    working on my comments

--- view/form.php
+++ view/form.php
@@ -3,7 +3,7 @@
 	require_once(__DIR__ . "/../model/config.php");
         require_once(__DIR__ . "/../controller/login-verify.php");
 
-// this if statement checks if users are authenticated
+// this if sta tement checks if users are authenticated
 // they also redirect people that are not registered to the index page
         if (!authenticateUser()) {
             header("Location: " . $path . "index.php");
@@ -15,7 +15,7 @@
 <form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
 	<div>
 		<label for="title"> Title: </label>
-		<input type="text" name="title" placeholder="Title" />
+		<input type="text" name="title" placeholder="Title"/>
 	</div>
 	<div>
 		<label for="post"> Post: </label>
