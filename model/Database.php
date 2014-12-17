4fa40536a6b0f9601c5b76ceaa6769b552c81a60
Miguel Garcia <miguelgarcia2515@gmail.com>
2014-12-17 00:17:54 -0800

    fixed comments

--- model/Database.php
+++ model/Database.php
@@ -1,6 +1,6 @@
 <?php
 // to create a class, you put classs then you put the name of the classs afterwards.
-// the class i am creating is going to take repeated code and make our code easier to read. A class is an object. A class can have variables inside. Classes store information so you can call it later. 
+// the class I am creating is going to take repeated code and make our code easier to read. A class is an object. A class can have variables inside. Classes store information so you can call it later. 
 class Database
 {
     private $connection;
